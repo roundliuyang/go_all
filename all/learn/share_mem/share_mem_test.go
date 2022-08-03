8a29089edc54bc576fb6567115de69eb4c4a9ff4
yly <liuyang.yuan>
2022-08-03 22:47:10 +0800

    共享内存并发机制

---
+++ all/learn/share_mem/share_mem_test.go
@@ -0,0 +1,57 @@
+package share_mem
+
+import (
+	"sync"
+	"testing"
+	"time"
+)
+
+// 并发访问共享变量，导致并发安全问题
+func TestCounter(t *testing.T) {
+
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		go func() {
+			counter++
+		}()
+	}
+	time.Sleep(1 * time.Second)
+	t.Logf("counter = %d", counter)
+}
+
+// 进行锁的保护（注意：这里sleep了一秒，不然无法得到正确的结果）
+func TestCounterThreadSafe(t *testing.T) {
+	var mut sync.Mutex
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		go func() {
+			defer func() {
+				mut.Unlock()
+			}()
+			mut.Lock()
+			counter++
+		}()
+	}
+	time.Sleep(1 * time.Second)
+	t.Logf("counter = %d", counter)
+}
+
+// WaitGroup,等待所有协程执行完
+func TestCounterWaitGroup(t *testing.T) {
+	var mut sync.Mutex
+	var wg sync.WaitGroup
+	counter := 0
+	for i := 0; i < 5000; i++ {
+		wg.Add(1)
+		go func() {
+			defer func() {
+				mut.Unlock()
+			}()
+			mut.Lock()
+			counter++
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	t.Logf("counter = %d", counter)
+}
