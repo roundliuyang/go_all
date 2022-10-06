9b50fc44af7a640cc0225aafcd33101c8e39ad95
yly <liuyang.yuan>
2022-10-06 17:51:23 +0800

    learn

--- all/learn/benchmark/concat _string_test.go
+++ all/learn/benchmark/concat _string_test.go
@@ -2,30 +2,30 @@ package benchmark
 
 import (
 	"bytes"
-	"github.com/stretchr/testify/assert"
+	//"github.com/stretchr/testify/assert"
 	"testing"
 )
 
-func TestConcatStringByAdd(t *testing.T) {
-	assert := assert.New(t)
-	elems := []string{"1", "2", "3", "4", "5"}
-	ret := ""
-	for _, elem := range elems {
-		ret += elem
-	}
-	assert.Equal("12345", ret)
-}
-
-func TestConcatStringByBytesBuffer(t *testing.T) {
-	assert := assert.New(t)
-	var buf bytes.Buffer
-	elems := []string{"1", "2", "3", "4", "5"}
-	for _, elem := range elems {
-		buf.WriteString(elem)
-
-	}
-	assert.Equal("12345", buf.String())
-}
+//func TestConcatStringByAdd(t *testing.T) {
+//	assert := assert.New(t)
+//	elems := []string{"1", "2", "3", "4", "5"}
+//	ret := ""
+//	for _, elem := range elems {
+//		ret += elem
+//	}
+//	assert.Equal("12345", ret)
+//}
+//
+//func TestConcatStringByBytesBuffer(t *testing.T) {
+//	assert := assert.New(t)
+//	var buf bytes.Buffer
+//	elems := []string{"1", "2", "3", "4", "5"}
+//	for _, elem := range elems {
+//		buf.WriteString(elem)
+//
+//	}
+//	assert.Equal("12345", buf.String())
+//}
 
 func BenchmarkConcatStringByAdd(b *testing.B) {
 
