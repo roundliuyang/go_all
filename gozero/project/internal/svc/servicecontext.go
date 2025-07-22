e52378aae447b5b405ad880d6cfc96712d1e7284
yly <2337352929@qq.com>
2025-07-22 20:49:34 +0800

    go-zero

--- gozero/project/internal/svc/servicecontext.go
+++ gozero/project/internal/svc/servicecontext.go
@@ -1,15 +1,20 @@
 package svc
 
 import (
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+	"hello01/db"
 	"hello01/internal/config"
 )
 
 type ServiceContext struct {
 	Config config.Config
+	Conn   sqlx.Session
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	sqlConn := db.NewMysql(c.MysqlConfig)
 	return &ServiceContext{
 		Config: c,
+		Conn:   sqlConn,
 	}
 }
